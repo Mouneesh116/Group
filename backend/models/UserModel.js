7e0376dde309bfae4f4cdca920f65e834356249f
Savanth <svinjamuri@loyaltymethods.com>
2025-09-01 14:21:07 +0530

    changed code

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -17,7 +17,7 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         minlength: 6,
-        maxlength:7
+        
     },
     role:{
         type: String,
